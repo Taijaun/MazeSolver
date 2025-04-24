ca324f0c6449790bcf0402ac9b13471e9434a96e
Taijaun Pitt <taijaun@hotmail.co.uk>
2025-04-24 18:12:19 +0100

    Created cell class

---
+++ src/cell.py
@@ -0,0 +1,31 @@
+from tkinter import Canvas
+from line import Line, Point
+
+class Cell():
+    def __init__(self, x1, x2, y1, y2, win):
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+        self._win = win
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+
+    def draw(self, top_left_x, top_left_y, bottom_right_x, bottom_right_y):
+        if self.has_left_wall:
+            left_wall = Line(Point(top_left_x, top_left_y), Point(top_left_x, bottom_right_y))
+            self._win.draw_line(left_wall, "purple")
+        
+        if self.has_right_wall:
+            right_wall = Line(Point(bottom_right_x, top_left_y), Point(bottom_right_x, bottom_right_y))
+            self._win.draw_line(right_wall, "purple")
+
+        if self.has_top_wall:
+            top_wall = Line(Point(top_left_x, top_left_y), Point(bottom_right_x, top_left_y))
+            self._win.draw_line(top_wall, "purple")
+
+        if self.has_bottom_wall:
+            bottom_wall = Line(Point(top_left_x, bottom_right_y), Point(bottom_right_x, bottom_right_y))
+            self._win.draw_line(bottom_wall, "purple")
--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from tkinter import Tk, BOTH, Canvas
 from line import Line, Point
+from cell import Cell
 
+# Class to handle the GUI
 class Window():
     def __init__(self, width, height):
         self.__width = width
@@ -33,15 +35,27 @@ def main():
     try:
         win = Window(800, 600)
 
-        point1 = Point(10, 10)
-        point2 = Point(100, 100)
-        line = Line(point1, point2)
+        cell_1 = Cell(50, 100, 50, 100, win)
+        cell_1.draw(cell_1._x1, cell_1._y1, cell_1._x2, cell_1._y2)
 
-        point3, point4 = Point(54, 85), Point(33, 55)
-        second_line = Line(point3, point4)
+        # Example 2: Remove the top wall
+        cell_2 = Cell(120, 180, 50, 100, win)  # A cell to the right of the first
+        cell_2.has_top_wall = False
+        cell_2.draw(cell_2._x1, cell_2._y1, cell_2._x2, cell_2._y2)
 
-        win.draw_line(line, "purple")
-        win.draw_line(second_line, "yellow")
+        # Example 3: Remove the left and bottom walls
+        cell_3 = Cell(50, 100, 120, 170, win)  # A cell below the first
+        cell_3.has_left_wall = False
+        cell_3.has_bottom_wall = False
+        cell_3.draw(cell_3._x1, cell_3._y1, cell_3._x2, cell_3._y2)
+
+        # Example 4: A cell with only the right wall
+        cell_4 = Cell(120, 180, 120, 170, win)
+        cell_4.has_left_wall = False
+        cell_4.has_top_wall = False
+        cell_4.has_bottom_wall = False
+        cell_4.draw(cell_4._x1, cell_4._y1, cell_4._x2, cell_4._y2)
+        
         win.wait_for_close()
         
         
